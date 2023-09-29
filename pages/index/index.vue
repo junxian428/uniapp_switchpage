05ed315eaadec793b946869f563439632eb6ca42
junxian428 <wengyin777@gmail.com>
2023-09-29 14:04:51 +0800

    first commit

---
+++ pages/index/index.vue
@@ -0,0 +1,49 @@
+<template>
+	   <div class="container">
+	     <div class="box">
+	       <h1><u>Hello World</u></h1>
+		   		<navigator url="/pages/index/list/list" open-type="switchTab" hover-class="other-navigator-hover">
+		   						<button type="default" @click="getDemo4">跳转tab页面</button>
+		   					</navigator>
+	     </div>
+	
+	   </div>
+</template>
+
+<script>
+	export default {
+		data() { 
+			return {
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			getDemo4(){
+				uni.navigateTo({
+					url:"/pages/index/list/list"
+				})
+			}
+		}
+	}
+</script>
+
+
+<style lang="scss">
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: flex-start; /* Aligns content at the top */
+  height: 100vh;
+}
+
+.box {
+  height: 100px;
+  background: pink;
+  h1 {
+    font-size: 40px;
+    color: red;
+  }
+}
+</style>
---
+++ pages/index/list/list.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<h2>New List page</h2>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
